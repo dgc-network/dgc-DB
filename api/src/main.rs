b37a2ab3ae43e995e3b650b975426eb0f31674e8
roverchen <rover.k.chen@gmail.com>
2020-05-28 10:56:43 +0800

    sabre-sdk = { version = "0.5", optional = true } sawtooth-sdk = { version = "0.4", features = ["transact-compat"] } scabbard = { git = "https://github.com/Cargill/splinter", optional = true, features = ["client", "events"] } serde = { version = "1.0", features = ["derive"] }

--- api/src/main.rs
+++ api/src/main.rs
@@ -2,6 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use crate::api::routes::{
+//    fetch_agent, fetch_grid_schema, fetch_organization, fetch_product, fetch_record,
+//    fetch_record_property, list_agents, list_grid_schemas, list_organizations,
+//    list_products, list_records, submit_batches,
+    get_batch_statuses, 
+};
 
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -17,8 +23,55 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .route("/", web::get().to(index))
             .route("/again", web::get().to(index2))
-    })
+            .service(web::resource("/batches").route(web::post().to(submit_batches)))
+            .service(
+                web::resource("/batch_statuses")
+                    .name("batch_statuses")
+                    .route(web::get().to(get_batch_statuses)),
+            )
+/*            
+            .service(
+                web::scope("/agent")
+                    .service(web::resource("").route(web::get().to(list_agents)))
+                    .service(
+                        web::resource("/{public_key}").route(web::get().to(fetch_agent)),
+                    ),
+            )
+            .service(
+                web::scope("/organization")
+                    .service(web::resource("").route(web::get().to(list_organizations)))
+                    .service(
+                        web::resource("/{id}").route(web::get().to(fetch_organization)),
+                    ),
+            )
+            .service(
+                web::scope("/product")
+                    .service(web::resource("").route(web::get().to(list_products)))
+                    .service(web::resource("/{id}").route(web::get().to(fetch_product))),
+            )
+            .service(
+                web::scope("/schema")
+                    .service(web::resource("").route(web::get().to(list_grid_schemas)))
+                    .service(
+                        web::resource("/{name}").route(web::get().to(fetch_grid_schema)),
+                    ),
+            )
+            .service(
+                web::scope("/record")
+                    .service(web::resource("").route(web::get().to(list_records)))
+                    .service(
+                        web::scope("/{record_id}")
+                            .service(web::resource("").route(web::get().to(fetch_record)))
+                            .service(
+                                web::resource("/property/{property_name}")
+                                    .route(web::get().to(fetch_record_property)),
+                            ),
+                    ),
+            )
+*/            
+})
 //    .bind("127.0.0.1:8088")?
+//    .bind(bind_url)?
     .bind("0.0.0.0:8088")?
     .run()
     .await
---
+++ api/src/routes/batches.rs
@@ -0,0 +1,116 @@
+// Copyright (c) The dgc.network
+// SPDX-License-Identifier: Apache-2.0
+
+use std::collections::HashMap;
+
+use actix_web::{web, HttpRequest, HttpResponse};
+use sawtooth_sdk::messages::batch::BatchList;
+use serde::Deserialize;
+
+use crate::api::error::RestApiResponseError;
+//use crate::rest_api::{AcceptServiceIdParam, AppState, QueryServiceId};
+//use crate::submitter::{BatchStatusResponse, BatchStatuses, SubmitBatches, DEFAULT_TIME_OUT};
+/*
+pub async fn submit_batches(
+    req: HttpRequest,
+    body: web::Bytes,
+    state: web::Data<AppState>,
+    query_service_id: web::Query<QueryServiceId>,
+    _: AcceptServiceIdParam,
+) -> Result<HttpResponse, RestApiResponseError> {
+    let batch_list: BatchList = match protobuf::parse_from_bytes(&*body) {
+        Ok(batch_list) => batch_list,
+        Err(err) => {
+            return Err(RestApiResponseError::BadRequest(format!(
+                "Protobuf message was badly formatted. {}",
+                err.to_string()
+            )));
+        }
+    };
+
+    let response_url = req.url_for_static("batch_statuses")?;
+
+    state
+        .batch_submitter
+        .submit_batches(SubmitBatches {
+            batch_list,
+            response_url,
+            service_id: query_service_id.into_inner().service_id,
+        })
+        .await
+        .map(|link| HttpResponse::Ok().json(link))
+}
+*/
+#[derive(Deserialize, Debug)]
+struct Params {
+    id: Vec<String>,
+}
+
+pub async fn get_batch_statuses(
+    req: HttpRequest,
+    state: web::Data<AppState>,
+    query: web::Query<HashMap<String, String>>,
+    query_service_id: web::Query<QueryServiceId>,
+    _: AcceptServiceIdParam,
+) -> Result<HttpResponse, RestApiResponseError> {
+    let batch_ids = match query.get("id") {
+        Some(ids) => ids.split(',').map(ToString::to_string).collect(),
+        None => {
+            return Err(RestApiResponseError::BadRequest(
+                "Request for statuses missing id query.".to_string(),
+            ));
+        }
+    };
+
+    // Max wait time allowed is 95% of network's configured timeout
+    let max_wait_time = (DEFAULT_TIME_OUT * 95) / 100;
+
+    let wait = match query.get("wait") {
+        Some(wait_time) => {
+            if wait_time == "false" {
+                None
+            } else {
+                match wait_time.parse::<u32>() {
+                    Ok(wait_time) => {
+                        if wait_time > max_wait_time {
+                            Some(max_wait_time)
+                        } else {
+                            Some(wait_time)
+                        }
+                    }
+                    Err(_) => {
+                        return Err(RestApiResponseError::BadRequest(format!(
+                            "Query wait has invalid value {}. \
+                             It should set to false or a time in seconds to wait for the commit",
+                            wait_time
+                        )));
+                    }
+                }
+            }
+        }
+
+        None => Some(max_wait_time),
+    };
+
+    let response_url = match req.url_for_static("batch_statuses") {
+        Ok(url) => format!("{}?{}", url, req.query_string()),
+        Err(err) => {
+            return Err(err.into());
+        }
+    };
+
+    state
+        .batch_submitter
+        .batch_status(BatchStatuses {
+            batch_ids,
+            wait,
+            service_id: query_service_id.into_inner().service_id,
+        })
+        .await
+        .map(|batch_statuses| {
+            HttpResponse::Ok().json(BatchStatusResponse {
+                data: batch_statuses,
+                link: response_url,
+            })
+        })
+}
