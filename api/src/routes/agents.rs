e31aa1f81840a31f24e902e4fdc6eb87bdeb2e0a
Rover <rover@RoverdeMacBook-Pro.local>
2020-06-03 20:26:43 +0800

    let sawtooth_connection = SawtoothConnection::new(&config.endpoint().url());

--- api/src/routes/agents.rs
+++ api/src/routes/agents.rs
@@ -372,7 +372,7 @@ pub async fn create_agent(
     //let state = AppState::new();
 
     //let sawtooth_connection = SawtoothConnection::new(&config.endpoint().url());
-    let sawtooth_connection = SawtoothConnection::new(Endpoint::url());
+    let sawtooth_connection = SawtoothConnection::new(&Endpoint::url());
     let batch_submitter = Box::new(SawtoothBatchSubmitter::new(
         sawtooth_connection.get_sender(),
     ));
