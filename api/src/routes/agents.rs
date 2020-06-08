fe71a44641e57b5240af6638e2e071ba6cf22a06
Rover <rover@RoverdeMacBook-Pro.local>
2020-06-08 22:58:50 +0800

    Ok(HttpResponse::Ok().body(wait.unwrap().to_string()))

--- api/src/routes/agents.rs
+++ api/src/routes/agents.rs
@@ -28,6 +28,17 @@ pub async fn list_agents(
     query: web::Query<HashMap<String, String>>,
 ) -> Result<HttpResponse, RestApiResponseError> {
 
+    // Get the Batch ID
+    let batch_ids = match query.get("id") {
+        Some(ids) => ids.split(',').map(ToString::to_string).collect(),
+        None => {
+            return Err(RestApiResponseError::BadRequest(
+                "Request for statuses missing id query.".to_string(),
+            ));
+        }
+    };
+    Ok(HttpResponse::Ok().body(batch_ids.unwrap().to_string()))
+/*
     // Max wait time allowed is 95% of network's configured timeout
     let max_wait_time = (DEFAULT_TIME_OUT * 95) / 100;
 
@@ -57,19 +68,9 @@ pub async fn list_agents(
 
         None => Some(max_wait_time),
     };
-
     Ok(HttpResponse::Ok().body(wait.unwrap().to_string()))
+*/    
 /*
-    // Get the Batch ID
-    let batch_ids = match query.get("id") {
-        Some(ids) => ids.split(',').map(ToString::to_string).collect(),
-        None => {
-            return Err(RestApiResponseError::BadRequest(
-                "Request for statuses missing id query.".to_string(),
-            ));
-        }
-    };
-
     // Get the URL
     let response_url = match req.url_for_static("agent") {
         Ok(url) => format!("{}?{}", url, req.query_string()),
