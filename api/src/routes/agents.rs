ab1d2f6bf8e2eb451cfe3fc11a2b9b04af646f46
roverchen <rover.k.chen@gmail.com>
2020-06-08 15:59:58 +0800

    req.query_string()

--- api/src/routes/agents.rs
+++ api/src/routes/agents.rs
@@ -169,7 +169,8 @@ pub async fn list_agents(
         Ok(url) => format!("{}?{}", url, req.query_string()),
         Err(err) => {
             //return Err(err.into());
-            return Err(RestApiResponseError::BadRequest("I am here.".to_string(),));
+            //return Err(RestApiResponseError::BadRequest("I am here.".to_string(),));
+            return Err(RestApiResponseError::BadRequest(req.query_string().to_string(),));
         }
     };
 
