7fee8492235e155db11a1e8946d17b747a4ccca2
Rover <rover@MacBook-Pro.local>
2020-06-11 20:21:05 +0800

    Ok(HttpResponse::Ok().body("Hello world! I am here to create_agent"))

--- api/src/routes/agents.rs
+++ api/src/routes/agents.rs
@@ -205,9 +205,6 @@ pub async fn create_agent(
         )?
         .create_batch_list();
 
-        Ok(HttpResponse::Ok().body("Hello world! I am here to create_agent"))
-
-/*
     let response_url = req.url_for_static("agent")?;
     
     let sawtooth_connection = SawtoothConnection::new(&response_url.to_string());
@@ -216,6 +213,9 @@ pub async fn create_agent(
         sawtooth_connection.get_sender(),
     ));
 
+    Ok(HttpResponse::Ok().body("Hello world! I am here to create_agent"))
+
+/*
     batch_submitter
         .submit_batches(SubmitBatches {
             batch_list,
