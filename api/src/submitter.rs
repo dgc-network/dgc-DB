8a0e63d05bea56834b63d7be1177b881a74096e5
Rover <rover@MacBook-Pro.local>
2020-06-13 22:30:59 +0800

    message_type

--- api/src/submitter.rs
+++ api/src/submitter.rs
@@ -191,7 +191,7 @@ pub fn query_validator<T: protobuf::Message, C: protobuf::Message, MS: MessageSe
     message_type: Message_MessageType,
     message: &C,
 ) -> Result<T, RestApiResponseError> {
-    Ok(protobuf::Message)
+    Ok(message_type)
     
 /*    
     let content = protobuf::Message::write_to_bytes(message).map_err(|err| {
