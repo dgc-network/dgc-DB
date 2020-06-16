5bfb8fc8db4f8babb59c4ddaf4a15ed28ffd0269
roverchen <rover.k.chen@gmail.com>
2020-06-16 14:12:00 +0800

    "Hello world! fetch_agent"

--- api/src/routes/agents.rs
+++ api/src/routes/agents.rs
@@ -70,9 +70,12 @@ pub async fn fetch_agent(
     let mut transaction_context = MockTransactionContext::default();
     let state = State::new(&mut transaction_context);
     let result = state.get_agent(&public_key).unwrap();
-    //let agent = result.unwrap();
+    let agent = match result.unwrap() {
+        Some(x) => result.unwrap(),
+        None => "Hello world! fetch_agent_value",
+    };
     //let org_id = agent.org_id();
-    let org_id = result.org_id();
+    //let org_id = result.org_id();
 
     Ok(HttpResponse::Ok().body("Hello world! fetch_agent"))
 
