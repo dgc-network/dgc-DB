db6b79e640a30a0c81859bebd8442a2e5740083f
roverchen <rover.k.chen@gmail.com>
2020-06-16 14:20:30 +0800

    "Hello world! fetch_agent"

--- api/src/routes/agents.rs
+++ api/src/routes/agents.rs
@@ -71,8 +71,8 @@ pub async fn fetch_agent(
     let state = State::new(&mut transaction_context);
     let result = state.get_agent(&public_key).unwrap();
     let agent = match result.unwrap() {
-        Some(x) => result.unwrap(),
-        None => "Hello world! fetch_agent_value",
+        x => result.unwrap(),
+        _ => Err("Hello world! fetch_agent_value"),
     };
     //let org_id = agent.org_id();
     //let org_id = result.org_id();
