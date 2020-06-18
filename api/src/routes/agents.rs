662d8019a7d474653a26046dec4d6e62d6e063f0
roverchen <rover.k.chen@gmail.com>
2020-06-18 16:16:16 +0800

    use sawtooth_sdk::signing::PrivateKey;

--- api/src/routes/agents.rs
+++ api/src/routes/agents.rs
@@ -6,6 +6,7 @@ use actix_web::{web, HttpRequest, HttpResponse};
 use sawtooth_sdk::signing::create_context;
 //use sawtooth_sdk::signing::Context;
 //use sawtooth_sdk::signing::secp256k1::Secp256k1Context;
+use sawtooth_sdk::signing::PrivateKey;
 use sawtooth_sdk::signing::secp256k1::Secp256k1PrivateKey;
 use serde::Deserialize;
 
@@ -186,7 +187,7 @@ pub async fn update_agent(
     let context = create_context("secp256k1")?;
     let private_key = Secp256k1PrivateKey::from_hex(&private_key_hex)?;
     //let public_key_hex = context.get_public_key(&private_key)?.as_hex();
-    let public_key = Box::into_raw(context.get_public_key(private_key)?).as_ref().unwrap();
+    let public_key = Box::into_raw(context.get_public_key(&private_key)?).as_ref().unwrap();
     println!("I am here! private_key = {:?}", private_key.as_hex());
     println!("I am here! public_key = {:?}", public_key.as_hex());
 
