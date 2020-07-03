4de09d23fffe7e348a9f769c206ae2e4b393a15b
roverchen <rover.k.chen@gmail.com>
2020-07-03 11:39:07 +0800

    let batch_list_bytes = batch_list         .write_to_bytes()         .expect("Error converting batch list to bytes");

--- api/src/routes/agents.rs
+++ api/src/routes/agents.rs
@@ -90,9 +90,9 @@ pub async fn create_agent(
     agent_input: web::Json<AgentInput>,
 ) -> Result<HttpResponse, RestApiResponseError> {
 
-    /// Client: Building and Submitting Transactions
+    // Client: Building and Submitting Transactions //
 
-    /// Creating a Private Key and Signer
+    // Creating a Private Key and Signer //
     //use sawtooth_sdk::signing::CryptoFactory;
     //use sawtooth_sdk::signing::create_context;
     
@@ -104,30 +104,87 @@ pub async fn create_agent(
     let crypto_factory = CryptoFactory::new(context.as_ref());
     let signer = crypto_factory.new_signer(private_key.as_ref());
 
-    /// Encoding Your Payload
-    //extern crate serde;
-    extern crate serde_cbor;
+    //let context = Secp256k1Context::new();
+    //let private_key = context.new_random_private_key()
+    //    .expect("Error generating a new Private Key");
+    let public_key = context.get_public_key(private_key.as_ref())
+        .expect("Error generating a new Public Key");
 
-    use serde::{Serialize, Deserialize};
+    // Encoding Your Payload //
+    //extern crate serde;
+    //extern crate serde_cbor;
+
+    //use serde::{Serialize, Deserialize};
 
     // Using serde to create a serializable struct
-    #[derive(Debug, Serialize, Deserialize)]
-    #[serde(rename_all = "PascalCase")]
-    struct Payload {
-        verb: String,
-        name: String,
-        value: u32,
-    }
+    //#[derive(Debug, Serialize, Deserialize)]
+    //#[serde(rename_all = "PascalCase")]
+    //struct Payload {
+    //    verb: String,
+    //    name: String,
+    //    value: u32,
+    //}
 
     // --snip--
-    let payload = Payload{  verb : String::from("set"),
-                            name : String::from("foo"),
-                            value : 42 };
+    //let payload = Payload{  verb : String::from("set"),
+    //                        name : String::from("foo"),
+    //                        value : 42 };
+
+    let org_id = &agent_input.org_id;
+    let roles_as_string = &agent_input.roles;
+    let metadata_as_string = &agent_input.metadata;
+
+    let mut roles = Vec::<String>::new();
+    for role in roles_as_string.chars() {
+        let entry: String = role.to_string().split(",").collect();
+        roles.push(entry.clone());
+    }
+
+    let mut metadata = Vec::<KeyValueEntry>::new();
+    for meta in metadata_as_string.chars() {
+        let meta_as_string = meta.to_string();
+        let key_val: Vec<&str> = meta_as_string.split(",").collect();
+        if key_val.len() != 2 {
+            "Metadata is formated incorrectly".to_string();            
+        }
+        let key = match key_val.get(0) {
+            Some(key) => key.to_string(),
+            None => "Metadata is formated incorrectly".to_string()
+        };
+        let value = match key_val.get(1) {
+            Some(value) => value.to_string(),
+            None => "Metadata is formated incorrectly".to_string()
+        };
+
+        let key_value = KeyValueEntryBuilder::new()
+            .with_key(key.to_string())
+            .with_value(value.to_string())
+            .build()
+            .unwrap();
+
+        metadata.push(key_value.clone());
+    }
+
+    let action = CreateAgentActionBuilder::new()
+        .with_org_id(org_id.to_string())
+        .with_public_key(public_key.as_hex())
+        .with_active(true)
+        .with_roles(roles)
+        .with_metadata(metadata)
+        .build()
+        .unwrap();
+
+    let payload = PikePayloadBuilder::new()
+        .with_action(Action::CreateAgent)
+        .with_create_agent(action)
+        .build()
+        .map_err(|err| RestApiResponseError::UserError(format!("{}", err)))?;
 
     let payload_bytes = serde_cbor::to_vec(&payload).expect("upsi");
 
-    /// Building the Transaction
-    /// 1. Create the Transaction Header
+    // Building the Transaction //
+/*    
+    // 1. Create the Transaction Header //
     extern crate protobuf;
     extern crate openssl;
     extern crate rand;
@@ -189,7 +246,7 @@ pub async fn create_agent(
         strs.join("")
     }
 
-    /// 2. Create the Transaction
+    // 2. Create the Transaction //
     use sawtooth_sdk::messages::transaction::Transaction;
 
     let signature = signer
@@ -202,7 +259,7 @@ pub async fn create_agent(
     txn.set_payload(payload_bytes);
     
 
-    /// 3. (optional) Encode the Transaction(s)
+    // 3. (optional) Encode the Transaction(s) //
     let txn_list_vec = vec![txn1, txn2];
     let txn_list = TransactionList::new();
     txn_list.set_transactions(RepeatedField::from_vec(txn_list_vec));
@@ -210,10 +267,11 @@ pub async fn create_agent(
     let txn_list_bytes = txn_list
         .write_to_bytes()
         .expect("Error converting Transaction List to bytes");
+*/
 
-    /// Building the Batch
-
-    /// 1. Create the BatchHeader
+    // Building the Batch //
+/*    
+    // 1. Create the BatchHeader //
     use sawtooth_sdk::messages::batch::BatchHeader;
 
     let mut batch_header = BatchHeader::new();
@@ -236,7 +294,7 @@ pub async fn create_agent(
         .write_to_bytes()
         .expect("Error converting batch header to bytes");
 
-    /// 2. Create the Batch
+    // 2. Create the Batch //
     use sawtooth_sdk::messages::batch::Batch;
 
     let signature = signer
@@ -249,7 +307,7 @@ pub async fn create_agent(
     batch.set_header_signature(signature);
     batch.set_transactions(RepeatedField::from_vec(vec![txn]));
 
-    /// 3. Encode the Batch(es) in a BatchList
+    // 3. Encode the Batch(es) in a BatchList //
     use sawtooth_sdk::messages::batch::BatchList;
 
     let mut batch_list = BatchList::new();
@@ -257,8 +315,26 @@ pub async fn create_agent(
     let batch_list_bytes = batch_list
         .write_to_bytes()
         .expect("Error converting batch list to bytes");
+*/
 
-    /// Submitting Batches to the Validator
+
+    let batch_list = BatchBuilder::new(
+        PIKE_FAMILY_NAME, 
+        PIKE_FAMILY_VERSION, 
+        &private_key.as_hex()
+    )
+    .add_transaction(
+        &payload.into_proto()?,
+        &[PIKE_NAMESPACE.to_string()],
+        &[PIKE_NAMESPACE.to_string()],
+    )?
+    .create_batch_list();
+
+    let batch_list_bytes = batch_list
+        .write_to_bytes()
+        .expect("Error converting batch list to bytes");
+
+    // Submitting Batches to the Validator //
     // When using an external crate don't forget to add it to your dependencies
     // in the Cargo.toml file, just like with the sdk itself
     extern crate reqwest;
@@ -272,19 +348,10 @@ pub async fn create_agent(
         )
         .send()
 
-    /// 
-
+    // ***** //
+/*
     let response_url = req.url_for_static("agent")?;
 
-    let org_id = &agent_input.org_id;
-    let roles_as_string = &agent_input.roles;
-    let metadata_as_string = &agent_input.metadata;
-
-    let context = Secp256k1Context::new();
-    let private_key = context.new_random_private_key()
-        .expect("Error generating a new Private Key");
-    let public_key = context.get_public_key(private_key.as_ref())
-        .expect("Error generating a new Public Key");
 
     println!("============ create_agent ============");
     println!("!dgc-network! private_key = {:?}", private_key.as_hex());
@@ -294,72 +361,11 @@ pub async fn create_agent(
     println!("!dgc-network! metadata = {:?}", metadata_as_string);
     println!("============ create_agent ============");
 
-    let mut roles = Vec::<String>::new();
-    for role in roles_as_string.chars() {
-        let entry: String = role.to_string().split(",").collect();
-        roles.push(entry.clone());
-    }
-
-    let mut metadata = Vec::<KeyValueEntry>::new();
-    for meta in metadata_as_string.chars() {
-        let meta_as_string = meta.to_string();
-        let key_val: Vec<&str> = meta_as_string.split(",").collect();
-        if key_val.len() != 2 {
-            "Metadata is formated incorrectly".to_string();            
-        }
-        let key = match key_val.get(0) {
-            Some(key) => key.to_string(),
-            None => "Metadata is formated incorrectly".to_string()
-        };
-        let value = match key_val.get(1) {
-            Some(value) => value.to_string(),
-            None => "Metadata is formated incorrectly".to_string()
-        };
-
-        let key_value = KeyValueEntryBuilder::new()
-            .with_key(key.to_string())
-            .with_value(value.to_string())
-            .build()
-            .unwrap();
-
-        metadata.push(key_value.clone());
-    }
-
-    let action = CreateAgentActionBuilder::new()
-        .with_org_id(org_id.to_string())
-        .with_public_key(public_key.as_hex())
-        .with_active(true)
-        .with_roles(roles)
-        .with_metadata(metadata)
-        .build()
-        .unwrap();
-
-    let payload = PikePayloadBuilder::new()
-        .with_action(Action::CreateAgent)
-        .with_create_agent(action)
-        .build()
-        .map_err(|err| RestApiResponseError::UserError(format!("{}", err)))?;
-
-    let batch_list = BatchBuilder::new(PIKE_FAMILY_NAME, PIKE_FAMILY_VERSION, &private_key.as_hex())
-        .add_transaction(
-            &payload.into_proto()?,
-            &[PIKE_NAMESPACE.to_string()],
-            &[PIKE_NAMESPACE.to_string()],
-        )?
-        .create_batch_list();
-
     //println!("!dgc-network! batch_list = {:?}", batch_list);
     //println!("!dgc-network! response_url = {:?}", response_url);
 
-    //let mock_sender = MockMessageSender::new(ResponseType::ClientBatchSubmitResponseOK);
-    //let mock_batch_submitter = Box::new(MockBatchSubmitter {
-    //    sender: mock_sender,
-    //});
-
-    //let batch_submitter = Box::new(SplinterBatchSubmitter::new(config.endpoint().url()));
     let batch_submitter = Box::new(SplinterBatchSubmitter::new(response_url.to_string()));
 
-    //mock_batch_submitter
     batch_submitter
         .submit_batches(SubmitBatches {
             batch_list,
@@ -368,7 +374,7 @@ pub async fn create_agent(
         })
         .await
         .map(|link| HttpResponse::Ok().json(link))
-
+*/
 
     //Ok(HttpResponse::Ok().body("Hello world! create_agent"))
 }
