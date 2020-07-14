01c42cc34953de0ea2ad89bd019660913e92943c
roverchen <rover.k.chen@gmail.com>
2020-07-14 12:24:45 +0800

    println!("!dgc-network! res = {:?}", res);

--- api/src/routes/organizations.rs
+++ api/src/routes/organizations.rs
@@ -66,12 +66,12 @@ pub async fn list_orgs(
 
     let json_res = json!(res);
     //let orgs = OrganizationList::from_bytes(json!(res)["data"]);
-    //let orgs = res.data;
+    let orgs = json_res.data;
 
     println!("============ list_org ============");
     println!("!dgc-network! res = {:?}", res);
     println!("!dgc-network! json_res = {:?}", json_res);
-    //println!("!dgc-network! orgs = {:?}", orgs);
+    println!("!dgc-network! orgs = {:?}", orgs);
 
     Ok(HttpResponse::Ok().body(res))
 
