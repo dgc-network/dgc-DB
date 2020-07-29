d80c00f7305630a6d7123778b9bab0a3fe7f827e
Rover <rover@MacBook-Pro.local>
2020-07-29 21:08:04 +0800

    println!("============ create_agent ============");     println!("!dgc-network! private_key = {:?}", private_key.as_hex());     println!("!dgc-network! public_key = {:?}", public_key.as_hex());     println!("!dgc-network! res = {:?}", res);
        Ok(HttpResponse::Ok().body(res))

--- api/src/routes/agents.rs
+++ api/src/routes/agents.rs
@@ -191,30 +191,18 @@ pub async fn create_agent(
     Ok(HttpResponse::Ok().body(res))
 }
 
-pub async fn update_agent(
-    //req: HttpRequest,
+fn do_create(
     input_data: web::Json<AgentData>,
-) -> Result<HttpResponse, RestApiResponseError> {
-/*
-    let response_url = req.url_for_static("agent")?;
+    private_key: &PrivateKey,
+) -> Result<String, RestApiResponseError> {
 
-    let private_key_as_hex = &input_data.private_key;
+    let public_key = context.get_public_key(private_key.as_ref())
+        .expect("Error generating a new Public Key");
+
+    // Creating the Payload //
     let org_id = &input_data.org_id;
     let roles_as_string = &input_data.roles;
     let metadata_as_string = &input_data.metadata;
-
-    let context = Secp256k1Context::new();
-    let private_key = Secp256k1PrivateKey::from_hex(&private_key_as_hex)
-        .expect("Error generating a new Private Key");
-    let public_key = context.get_public_key(&private_key)
-        .expect("Error generating a new Public Key");
-
-    println!("============ update_agent ============");
-    println!("!dgc-network! private_key = {:?}", private_key.as_hex());
-    println!("!dgc-network! public_key = {:?}", public_key.as_hex());
-    println!("!dgc-network! org_id = {:?}", org_id);
-    println!("!dgc-network! roles = {:?}", roles_as_string);
-    println!("!dgc-network! metadata = {:?}", metadata_as_string);
 
     let mut roles = Vec::<String>::new();
     for role in roles_as_string.chars() {
@@ -247,7 +235,7 @@ pub async fn update_agent(
         metadata.push(key_value.clone());
     }
 
-    let action = UpdateAgentActionBuilder::new()
+    let action = CreateAgentActionBuilder::new()
         .with_org_id(org_id.to_string())
         .with_public_key(public_key.as_hex())
         .with_active(true)
@@ -257,40 +245,59 @@ pub async fn update_agent(
         .unwrap();
 
     let payload = PikePayloadBuilder::new()
-        .with_action(Action::UpdateAgent)
-        .with_update_agent(action)
+        .with_action(Action::CreateAgent)
+        .with_create_agent(action)
         .build()
         .map_err(|err| RestApiResponseError::UserError(format!("{}", err)))?;
 
-    let batch_list = BatchBuilder::new(PIKE_FAMILY_NAME, PIKE_FAMILY_VERSION, &private_key.as_hex())
-        .add_transaction(
-            &payload.into_proto()?,
-            &[PIKE_NAMESPACE.to_string()],
-            &[PIKE_NAMESPACE.to_string()],
-        )?
-        .create_batch_list();
+    // Building the Transaction and Batch//
+    let batch_list = BatchBuilder::new(
+        PIKE_FAMILY_NAME, 
+        PIKE_FAMILY_VERSION, 
+        &private_key.as_hex()
+    )
+    .add_transaction(
+        &payload.into_proto()?,
+        &[PIKE_NAMESPACE.to_string()],
+        &[PIKE_NAMESPACE.to_string()],
+    )?
+    .create_batch_list();
 
-    println!("!dgc-network! batch_list = {:?}", batch_list);
-    println!("!dgc-network! response_url = {:?}", response_url);
+    let batch_list_bytes = batch_list
+        .write_to_bytes()
+        .expect("Error converting batch list to bytes");
 
-    //let mock_sender = MockMessageSender::new(ResponseType::ClientBatchSubmitResponseOK);
-    //let mock_batch_submitter = Box::new(MockBatchSubmitter {
-    //    sender: mock_sender,
-    //});
+    // Submitting Batches to the Validator //
+    let res = reqwest::Client::new()
+        .post("http://rest-api:8008/batches")
+        .header("Content-Type", "application/octet-stream")
+        .body(batch_list_bytes)
+        .send()
+        .await?
+        .text()
+        .await?;
 
-    //let batch_submitter = Box::new(SplinterBatchSubmitter::new(config.endpoint().url()));
-    let batch_submitter = Box::new(SplinterBatchSubmitter::new(response_url.to_string()));
+    return res;
+}
 
-    //mock_batch_submitter
-    batch_submitter
-        .submit_batches(SubmitBatches {
-            batch_list,
-            response_url,
-            //service_id: query_service_id.into_inner().service_id,
-        })
-        .await
-        .map(|link| HttpResponse::Ok().json(link))
-*/
+pub async fn update_agent(
+    //req: HttpRequest,
+    input_data: web::Json<AgentData>,
+) -> Result<HttpResponse, RestApiResponseError> {
 
-    Ok(HttpResponse::Ok().body("Hello world! create_agent"))
+    // Creating a Private Key and Signer //
+    let private_key_as_hex = &input_data.private_key;
+    let private_key = Secp256k1PrivateKey::from_hex(&private_key_as_hex)
+        .expect("Error generating a new Private Key");
+
+    let res = do_create(input_data, private_key);
+
+    println!("============ update_agent ============");
+    //println!("!dgc-network! private_key = {:?}", private_key.as_hex());
+    //println!("!dgc-network! public_key = {:?}", public_key.as_hex());
+    println!("!dgc-network! res = {:?}", res);
+
+    Ok(HttpResponse::Ok().body(res))
+    
+    //Ok(HttpResponse::Ok().body("Hello world! update_agent"))
 }
