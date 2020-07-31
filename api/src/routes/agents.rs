af4e3cb4150c71d6d704bf14bc7d364d72d8375c
roverchen <rover.k.chen@gmail.com>
2020-07-31 17:09:07 +0800

    let public_key = context.get_public_key(&private_key_new);

--- api/src/routes/agents.rs
+++ api/src/routes/agents.rs
@@ -223,12 +223,12 @@ fn do_batches(
 
     if action_plan == Action::CreateAgent {
 
-        let private_key_new = context.new_random_private_key()
-        .expect("Error generating a new Private Key");
+        let private_key_new = context.new_random_private_key();
+        //.expect("Error generating a new Private Key");
         //let private_key_as_hex = private_key_new.as_hex();
         //let private_key = Secp256k1PrivateKey::from_hex(&private_key_as_hex);
-        let public_key = context.get_public_key(&private_key_new)
-        .expect("Error generating a new Public Key");
+        let public_key = context.get_public_key(&private_key_new);
+        //.expect("Error generating a new Public Key");
 
         let action = CreateAgentActionBuilder::new()
         .with_org_id(org_id.to_string())
