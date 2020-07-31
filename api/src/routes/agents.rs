caeef3e21f7c2305d2aef1c7a3a99dba327fc9a9
roverchen <rover.k.chen@gmail.com>
2020-07-31 16:36:50 +0800

    let context = create_context("secp256k1")         .expect("Error creating the right context");

--- api/src/routes/agents.rs
+++ api/src/routes/agents.rs
@@ -182,12 +182,11 @@ fn do_batches(
 ) -> Result<Vec<u8>, RestApiResponseError> {
 
     // Creating a Private Key and Signer //
+    let context = create_context("secp256k1")
+        .expect("Error creating the right context");
     let private_key_as_hex = &input_data.private_key;
     let private_key = Secp256k1PrivateKey::from_hex(&private_key_as_hex)
         .expect("Error generating a new Private Key");
-
-    let context = create_context("secp256k1")
-        .expect("Error creating the right context");
 
     // Creating the Payload //
     let org_id = &input_data.org_id;
@@ -231,7 +230,9 @@ fn do_batches(
 
         let private_key_new = context.new_random_private_key()
         .expect("Error generating a new Private Key");
-        let public_key = context.get_public_key(&private_key_new)
+        let private_key_as_hex = private_key_new.as_hex();
+        let private_key = Secp256k1PrivateKey::from_hex(&private_key_as_hex)
+        let public_key = context.get_public_key(&private_key)
         .expect("Error generating a new Public Key");
 
         let action = CreateAgentActionBuilder::new()
