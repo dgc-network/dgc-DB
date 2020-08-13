6c2df33343f9bd7da3fffb6ddf0db54a541085c3
Rover <rover@MacBook-Pro.local>
2020-08-13 21:15:09 +0800

    .chars()

--- api/src/routes/schemas.rs
+++ api/src/routes/schemas.rs
@@ -165,7 +165,7 @@ fn do_batches(
 
     let mut properties = Vec::<PropertyDefinition>::new();
     //for meta in properties_as_string.chars() {
-    for meta in input_data.properties {
+    for meta in input_data.properties.chars() {
         //let meta_as_string = meta.to_string();
         //let key_val: Vec<&str> = meta_as_string.split(",").collect();
         let key_val: Vec<&str> = meta.split(",").collect();
