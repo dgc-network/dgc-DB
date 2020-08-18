c5d99514c3f83071343b9daa43d0e21ba121e2a8
roverchen <rover.k.chen@gmail.com>
2020-08-18 12:28:05 +0800

    _builder

--- api/src/routes/records.rs
+++ api/src/routes/records.rs
@@ -217,13 +217,13 @@ fn retrieve_property_values(
     let vec: Vec<&str> = properties_as_string.split(",").collect();
     let key_val_vec = split_vec(vec, 9);
     for key_val in key_val_vec {
+        let property_value_builder = PropertyValueBuilder::new();
         if key_val.len() != 9 {
             "Properties are formated incorrectly".to_string();            
         }
-        let mut property_value = PropertyValueBuilder::new();
         let name = match key_val.get(0) {
             Some(value) => {
-                property_value.with_name(value.to_string());
+                property_value_builder.with_name(value.to_string());
                 value.to_string()
             },
             None => "name is formated incorrectly".to_string()
@@ -242,7 +242,7 @@ fn retrieve_property_values(
             
             None => Some(DataType::Bytes)
         };
-        property_value.with_data_type(data_type.unwrap());
+        property_value_builder.with_data_type(data_type.unwrap());
 
         if data_type.unwrap() == DataType::Bytes {
             let bytes_value = match key_val.get(2) {
@@ -268,7 +268,7 @@ fn retrieve_property_values(
         if data_type.unwrap() == DataType::String {
             let string_value = match key_val.get(5) {
                 Some(value) => {
-                    property_value.with_string_value(value.to_string());
+                    property_value_builder.with_string_value(value.to_string());
                     value.to_string()
                 },
                 None => "description is formated incorrectly".to_string()
@@ -300,7 +300,7 @@ fn retrieve_property_values(
         //.with_name(name.into())
         //.with_data_type(data_type.unwrap())
         //.with_number_value(number_value.unwrap())
-        property_value.build()
+        let property_value = property_value_builder.build()
         .unwrap();
 
         properties.push(property_value.clone());
